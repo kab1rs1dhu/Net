bf65a93ddc56361aeeb78f42fb46dd5423d821a9
Kabir Sidhu <kxbir414@gmail.com>
2025-09-29 10:27:08 -0700

    added all necessary files and folders and removed the old structure

---
+++ mobile/lib/utils.js
@@ -0,0 +1,10 @@
+// lib/utils.js
+export function formatDate(dateString) {
+  // format date nicely
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
